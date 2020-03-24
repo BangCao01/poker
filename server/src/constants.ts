044ab36a4c6c464dd889d7efb3a855620f318a5e
BangCao01 <43467569+BangCao01@users.noreply.github.com>
2020-03-24 19:53:37 +0700

    - delete

--- server/src/constants.ts
+++
@@ -1,9 +0,0 @@
-export enum ChatEvent {
-  CONNECT = 'connect',
-  DISCONNECT = 'disconnect',
-  MESSAGE = 'message',
-  LOGIN='login',
-  KICK='kick',
-  CHIABAI='chiabai',
-  DANHBAI='danhbai',
-}
--- server/src/server.ts
+++
@@ -1,7 +0,0 @@
-// src/server.ts
-
-import { ChatServer } from './ChatServer';
-
-let app = new ChatServer().app;
-
-export { app };
